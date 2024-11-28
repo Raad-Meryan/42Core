6617fcf3c079069f2e81183fffed0dd304dbf3a7
Raad Meryan <rmeryan@c1r1s1.42amman.com>
2024-11-28 13:34:56 +0300

    Fix Spacing

--- ft_printf/ft_helpers.c
+++ ft_printf/ft_helpers.c
@@ -2,34 +2,34 @@
 
 void ft_memset(void *s, int c, size_t n)
 {
-    byte    *ptr;
+	byte	*ptr;
 
-    ptr = (byte *)s;
-    while (n)
-    {
-        *ptr++ = (byte)c;
-        --n;
-    }
+	ptr = (byte *)s;
+	while (n)
+	{
+		*ptr++ = (byte)c;
+		--n;
+	}
 }
 
 int in(const char *s, char c)
 {
-    if(!s)
-        return (0);
-    while (*s)
-    {
-        if (*s == c)
-            return (1);
-        s++;
-    }
+	if(!s)
+		return (0);
+	while (*s)
+	{
+		if (*s == c)
+			return (1);
+		s++;
+	}
 }
 
 int	ft_atoi(t_data *data)
 {
 	int	value;
 
-    value = 0;
-    while (in(NUMBERS, *data->s))
-        value = value * 10 + (*data->s++ - '0');
-    return (value);
+	value = 0;
+	while (in(NUMBERS, *data->s))
+		value = value * 10 + (*data->s++ - '0');
+	return (value);
 }
--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -2,41 +2,41 @@
 
 static int init_data(t_data *data, const char *format)
 {
-    data->s = format;
-    data->chars_written = 0;
-    data->buffer = (char *)malloc(BUFFER_SIZE);
-    if (!data->buffer)
-        return (-1);
-    data->buffer_index = 0;
-    ft_memset(data->buffer, 0, BUFFER_SIZE * sizeof(char));
-    return (0);
+	data->s = format;
+	data->chars_written = 0;
+	data->buffer = (char *)malloc(BUFFER_SIZE);
+	if (!data->buffer)
+		return (-1);
+	data->buffer_index = 0;
+	ft_memset(data->buffer, 0, BUFFER_SIZE * sizeof(char));
+	return (0);
 }
 int ft_printf(const char *format, ...)
 {
-    t_data  data;
+	t_data  data;
 
-    va_start(data.args_pointer, format);
-    if (init_data(&data, format))
-        return (-1);
-    while (*data.s)
-    {
-        // check if we have a format specifier
-        if (*data.s == '%' && *(++data.s))
-        {
-            // parse the format specifier
-            if (parse_format(&data))
-                return (-1);
-            render_format(&data);
-        }
-        else
-        {
-            // write the character to the buffer
-            write_to_buff(&data, *data.s);
-        }
-        data.s++;
-    }
-    flush_bufff(&data);
-    va_end(data.args_pointer);
-    free(data.buffer);
-    return (data.chars_written);
+	va_start(data.args_pointer, format);
+	if (init_data(&data, format))
+		return (-1);
+	while (*data.s)
+	{
+		// check if we have a format specifier
+		if (*data.s == '%' && *(++data.s))
+		{
+			// parse the format specifier
+			if (parse_format(&data))
+				return (-1);
+			render_format(&data);
+		}
+		else
+		{
+			// write the character to the buffer
+			write_to_buff(&data, *data.s);
+		}
+		data.s++;
+	}
+	flush_bufff(&data);
+	va_end(data.args_pointer);
+	free(data.buffer);
+	return (data.chars_written);
 }
