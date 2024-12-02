eb838ec7accd1d499d574bb5674da1465e19e57d
Raad Meryan <rmeryan@c2r1s7.42amman.com>
2024-12-02 18:22:41 +0300

    numbers

--- ft_printf/ft_renderNum.c
+++ ft_printf/ft_renderNum.c
@@ -7,9 +7,9 @@ static int	add_prefix(t_data *data, long num, char *buffer, int unsignedNum)
 	len = 0;
 	if (num < 0 && !unsignedNum)
 		buffer[len++] = '-';
-	else if (data->flags.plus)
+	else if (data->flags.plus && !unsignedNum)
 		buffer[len++] = '+';
-	else if (data->flags.space)
+	else if (data->flags.space && !unsignedNum)
 		buffer[len++] = ' ';
 	if (data->flags.hash && num != 0)
 	{
@@ -78,30 +78,61 @@ static void	handle_padding(t_data *data, int len, int content, char pad_char)
 		putchar_buff_n(pad_char, padding, data);
 }
 
+unsigned long calc_abs_value(long num, int is_signed)
+{
+	if (is_signed && num < 0)
+		return ((unsigned long)(-num));
+	else
+		return ((unsigned long)num);
+}
+
+int handle_precision(t_data *data, int num_len)
+{
+	int	zero_pad;
+
+	zero_pad = 0;
+	if (data->flags.precision >= 0)
+	{
+		data->flags.zero_pad = 0;
+		zero_pad = data->flags.precision - num_len;
+		if (zero_pad < 0)
+			zero_pad = 0;
+	}
+	return (zero_pad);
+}
+
+void handle_width_padding (t_data *data, int content_len)
+{
+	if (!data->flags.left_justified && data->flags.width > content_len)
+		{
+			if (data->flags.zero_pad == 1)
+				handle_padding(data, data->flags.width, content_len, '0');
+			else
+				handle_padding(data, data->flags.width, content_len, ' ');
+		}
+}
+
 void	print_num(t_data *data, long num, int is_signed)
 {
-	char	buffer[BUFFER_SIZE];
-	int		content_len;
-	int		prefix_len;
-	char	*x;
+	char			buffer[BUFFER_SIZE];
+	int				content_len;
+	int				prefix_len;
+	int				num_len;
+	int				zero_pad;
+	char			*x;
 	unsigned long	abs_num;
 
 	prefix_len = add_prefix(data, num, buffer, !is_signed);
 	x = prefix_len + buffer;
-	if (is_signed && num < 0)
-		abs_num = (unsigned long)(-num);
-	else
-		abs_num = (unsigned long)num;
+	abs_num = calc_abs_value(num, is_signed);
 	base_convert(abs_num, data->flags.base, data->flags.uppercase, x);
-	content_len = str_len(buffer);
-	if (!data->flags.left_justified)
-	{
-		if (data->flags.zero_pad == 1)
-			handle_padding(data, data->flags.width, content_len, '0');
-		else //if (data->flags.zero_pad == 0)
-			handle_padding(data, data->flags.width, content_len, ' ');
-	}
-	putstr_buff_n(buffer, content_len, data);
+	num_len = str_len(x);
+	zero_pad = handle_precision(data, num_len);
+	content_len = prefix_len + zero_pad + num_len;
+	handle_width_padding(data, content_len);
+	putstr_buff_n(buffer, prefix_len, data);
+	putchar_buff_n('0', zero_pad, data);
+	putstr_buff_n(x, num_len, data);
 	if (data->flags.left_justified)
 		handle_padding(data, data->flags.width, content_len, ' ');
 }
